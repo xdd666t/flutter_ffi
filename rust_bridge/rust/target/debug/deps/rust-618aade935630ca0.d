1dbb0f42eb8da0a5f46938ce100cd0df9318618f
小呆呆666 <xdd666@foxmail.com>
2022-11-26 19:59:11 +0800

    init rust project

---
+++ rust_bridge/rust/target/debug/deps/rust-618aade935630ca0.d
@@ -0,0 +1,5 @@
+F:\GitHub\flutter_ffi\rust_bridge\rust\target\debug\deps\rust-618aade935630ca0.exe: src\lib.rs
+
+F:\GitHub\flutter_ffi\rust_bridge\rust\target\debug\deps\rust-618aade935630ca0.d: src\lib.rs
+
+src\lib.rs:
