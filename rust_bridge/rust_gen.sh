87fbbd52867c00f4ebbb7c75618e8cd024c3b5dd
小呆呆666 <xdd666t@gmail.com>
2022-12-11 21:03:12 +0800

    Update rust_gen.sh

--- rust_bridge/rust_gen.sh
+++ rust_bridge/rust_gen.sh
@@ -1,6 +1,6 @@
 # 生成指令
 flutter_rust_bridge_codegen -r rust/src/api.rs -d lib/ffi/rust_ffi/rust_ffi.dart -c ios/Runner/bridge_generated.h -c macos/Runner/bridge_generated.h
 
-# 执行该命令: bash rust_gen.sh
+# macos执行该命令: bash rust_gen.sh
 
 
